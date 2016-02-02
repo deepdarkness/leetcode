bcec9fcb183de3f7a080694b6892a44ea31a5830
zz <zhaozhuo1112@hotmail.com>
2016-02-03 01:06:42 +0800

    update new answer #179

---
+++ code/Largest_Number.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def largestNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: str
+        """
+
+        def cmpfunc(x1, x2):
+            if int(x1 + x2) > int(x2 + x1):
+                return 1
+            else:
+                return -1
+
+        nums = map(str, nums)
+        nums.sort(cmpfunc, reverse=True)
+        tmp = ''.join(nums).lstrip("0")
+        if not tmp:
+            tmp = "0"
+        return tmp
+
+
+if __name__ == '__main__':
+    s = Solution()
+    print s.largestNumber([3, 30, 34, 5, 9])
