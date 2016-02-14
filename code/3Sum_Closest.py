e7265435c5e4585f1d82854d5d55acaad7885c52
zz <zhaozhuo1112@hotmail.com>
2016-02-15 00:12:12 +0800

    update new answer

--- code/3Sum_Closest.py
+++ code/3Sum_Closest.py
@@ -8,16 +8,24 @@ class Solution(object):
         nums.sort()
         l = len(nums)
         a = b = c = 0
-        ans=target
+        ans = target
         diff = float('inf')
         while a < l - 2:
             b = a + 1
             c = l - 1
-            s = nums[a] + nums[b] + nums[c]
-            if s == target:
-                return target
-            elif s>target:
-                if s-target<diff:
-                    diff=s-target
-                    ans=s
-
+            while b < c:
+                s = nums[a] + nums[b] + nums[c]
+                if s == target:
+                    return target
+                elif s > target:
+                    if s - target < diff:
+                        diff = s - target
+                        ans = s
+                    c -= 1
+                else:
+                    if target - s < diff:
+                        diff = target - s
+                        ans = s
+                    b += 1
+            a += 1
+        return ans
