84b10456c39f811a8b60d5c1ee049150d7121354
zz <zhaozhuo1112@hotmail.com>
2016-02-19 02:08:27 +0800

    update new answer

---
+++ code/Generate_Parentheses.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def generateParenthesis(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        ans = []
+
+        def travel(current, lleft, rleft):
+            if lleft + rleft == 0:
+                ans.append(current)
+                return
+            if lleft == rleft and lleft > 0:
+                travel(current + '(', lleft - 1, rleft)
+                return
+            if rleft > lleft > 0:
+                travel(current + ')', lleft, rleft - 1)
+                travel(current + '(', lleft - 1, rleft)
+            elif rleft > lleft == 0:
+                travel(current + ')', lleft, rleft - 1)
+
+        travel('', n, n)
+        return ans
+
+if __name__ == '__main__':
+    s=Solution()
+    print s.generateParenthesis(1)
